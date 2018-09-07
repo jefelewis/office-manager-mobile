178dab55179cd3cd2edf940a7908b7b195852ca7
Jeff Lewis <jeffylew77@gmail.com>
2018-09-07 12:22:20 -0700

    [Dev] Adding Firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import firebaseConfig from '../config/config';
 
 // Imports: Components
 
@@ -13,6 +15,11 @@ import reducers from './reducers';
 
 // React Native Component
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
