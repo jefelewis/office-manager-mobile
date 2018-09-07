e47a8152793739cbfc5a2d22c514dc3a4c987eda
Jeff Lewis <jeffylew77@gmail.com>
2018-09-07 13:24:02 -0700

    [Dev] Adding styles

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,21 +1,49 @@
 // Imports: Dependencies
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 
 
 // React Native Component
 class LoginForm extends Component {
+  componentWillMount() {
+    console.log('Login Form Mounted');
+  }
+
   render() {
     return (
-      <Card>
+      <Card style={styles.cardStyle}>
         <CardSection>
-          <Text>Hi</Text>
+          <Input
+            label="Email"
+            placeholder="Email"
+          />
         </CardSection>
+
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="Password"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+
       </Card>
     );
   }
 }
+
+
+// Styles
+const styles = StyleSheet.create({
+  cardStyle: {
+    flex: 1,
+  },
+});
 
 
 // Exports
